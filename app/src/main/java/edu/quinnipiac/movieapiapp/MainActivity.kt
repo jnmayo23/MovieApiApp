8e2407af9ea68ddd4d899098085b60a08040e9e6
jnmayo23 <Jordan.Mayo@quinnipiac.edu>
2023-03-27 22:31:41 -0400

    Action Bar, Navigation, and Help Fragment

---
+++ app/src/main/java/edu/quinnipiac/movieapiapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package edu.quinnipiac.movieapiapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import edu.quinnipiac.movieapiapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        setSupportActionBar(binding.toolbar)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val builder = AppBarConfiguration.Builder(navController.graph)
+        builder.setOpenableLayout(drawer)
+        val appBarConfiguration = builder.build()
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+    }
+
+}
